e39d916386218e6330dc5963755b9c1bdb2e126e
ahig1 <ahigps@gmail.com>
2026-03-29 19:08:41 +0300

    fix3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/max_bot_miniapp/',
   plugins: [react(), sentryVitePlugin({
     org: "avtoto-chatbot",
     project: "javascript-react"
